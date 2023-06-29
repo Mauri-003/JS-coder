6f7401d17308d32a74a5bf7450dccc0d1f852a4c
mauri molina <maurimolina003@gmail.com>
2023-06-29 20:21:51 -0300

    actualizaion del formulario JS

--- JS/app.js
+++ JS/app.js
@@ -1,17 +1,45 @@
 
-const datosPersonales = document.getElementById('miFormulario');
+ let form = document.getElementById("byform");
+ let submitbutton = document.getElementById("submitbutton");
 
+   submitbutton.addEventListener("click",function(e){
+    e.preventDefault()
 
-  const nombre = document.getElementById("nombre").value;
-  const email = document.getElementById("email").value;
-  const tel = document.getElementById("tel").value;
+  let nombre = document.getElementById("nombre").value;
+  let email = document.getElementById("email").value;
+  let tel = document.getElementById("tel").value;
+  let comentario = document.getElementById("comentario").value;
+  let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (nombre.length > 3){
+        alert("nombre valido")
+     }else{
+        alert("nombre es my corto")
+     }
+    if (emailRegex.test(email)) {
+      alert("El correo electrónico es válido.");
+    } else {
+      alert("El correo electrónico no es válido.");
+     }
+     function validarNumeroTelefono(tel) {
+        let patron = /^\d{2}-\d{4}-\d{6}$/;
+        
+        if (patron.test(tel)) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+      
+      let numeroTel = "54-4564-789099";
+      if (validarNumeroTelefono(numeroTel)) {
+        console.log("El número de teléfono es válido.");
+      } else {
+        console.log("El número de teléfono no es válido.");
+      }
+    
  
-
- 
- function datosPersonales2(nombre,email,tel,){
-    if (nombre ==="" || email ==="" || tel ===""){
-        console.log ("Faltan datos personales")
-    }
-
-}
-datosPersonales2(nombre,email,tel);
+          
+git
+})
+  
